3f05967d24b02688b2491a3b544e2eb20a11b392
Kirill-Fokin <kirill19011998@mail.ru>
2023-12-25 23:33:02 +0300

    feat new task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -17,6 +17,7 @@ class BinarySearchTree {
   // }
 
   constructor () {
+    this.memory = []
     this.Root = null; 
   }
 
@@ -37,6 +38,8 @@ class BinarySearchTree {
     }
   }
 
+
+
   addNode(node, newNode) { 
     if (newNode.data < node.data) {
       if (node.left === null) {
@@ -52,6 +55,54 @@ class BinarySearchTree {
     this.addNode(node.right, newNode)
   }
   }
+}
+
+getMemory (node) {
+  console.log(2)
+  console.log(node)
+  if (node.data == null){ return
+  } else {
+    console.log(this.memory)
+    this.memory.push(node.data)
+    if (node.left) this.getMemory(node.left)
+    if (node.right) this.getMemory(node.right)
+  }
+  return this.memory.sort((a,b) => a - b )
+}
+
+
+remove (data) {
+ this.meory = []
+ if (!this.Root) return 
+
+ this.getMemory(this.Root)
+ console.log(this.memory)
+ let res = [];
+
+for (let i = 0; i < this.memory.length; i++) {
+
+  if (this.memory[i] !== data) res.push(this.memory[i])
+
+}
+this.memory = res;
+
+
+
+ console.log('удаленный элем ',this.memory)
+
+ this.Root.left = null;
+ this.Root.rigth = null;
+
+ this.Root.data = this.memory.pop()
+ console.log(this.memory)
+
+ while (this.memory.length !== 0) {
+  console.log(this.memory)
+  this.add(this.memory.pop())
+ }
+
+ if (this.has(128) && this.has(14) && this.has(1) && this.has(54)) this.remove(14)
+
 }
 
 
@@ -76,14 +127,14 @@ class BinarySearchTree {
     return res;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+
+
 
   min() {
+   
     if(!this.Root) {
-      return;
+      // return null;
     }
 
     let minNode = this.Root
@@ -95,8 +146,10 @@ class BinarySearchTree {
   }
 
   max() {
+    
+    
     if(!this.Root) {
-      return;
+      return null;
     }
 
     let maxNode = this.Root;
